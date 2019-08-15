d99876bc1f904a141cfeb727cde7c6cc1784f597
Neal JIn Oh <no4259@gmail.com>
2019-08-15 14:13:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_piece_owned => 50,
-        
+        :three_pieces_owned => 
         }
       }
     }
